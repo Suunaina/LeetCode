f228fe1d4a95858ff3c13e9954c247aaad3833d3
Sunaina Ravi <sunainaravi10@gmail.com>
2026-07-20 23:04:28 +0530

    Time: 2 ms (99.3%), Space: 44.2 MB (75.29%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j){
+
+            char left = Character.toLowerCase(s.charAt(i));
+            char right = Character.toLowerCase(s.charAt(j));
+
+            if(!Character.isLetterOrDigit(s.charAt(i))){
+                i++;
+                continue;
+            }
+
+            if(!Character.isLetterOrDigit(s.charAt(j))){
+                j--;
+                continue;
+            }
+
+            if(left == right){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
