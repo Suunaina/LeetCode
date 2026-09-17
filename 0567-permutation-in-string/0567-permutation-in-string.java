c7313c457507db880e54e889f61817ff4320af16
Sunaina Ravi <sunainaravi10@gmail.com>
2026-09-18 00:00:50 +0530

    Time: 7 ms (66.92%), Space: 43.8 MB (92.93%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length()) return false;
+
+        int[] freq = new int[26];
+        int[] window = new int[26];
+
+        for (char c : s1.toCharArray()) {
+            freq[c - 'a']++;
+        }
+
+        int left = 0;
+
+        for (int right = 0; right < s2.length(); right++) {
+            window[s2.charAt(right) - 'a']++;
+
+            if (right - left + 1 > s1.length()) {
+                window[s2.charAt(left) - 'a']--;
+                left++;
+            }
+
+            if (Arrays.equals(freq, window)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
